420c2cfe6e46bf6b1911b13573b253da7abf26b7
vangelis-z <vangelis.zacharis@disroot.org>
2024-10-18 14:29:32 +0300

    Added optional support for `plotly`

--- src/gnss_tools/plot_groundtrack.py
+++ src/gnss_tools/plot_groundtrack.py
@@ -4,69 +4,26 @@
 
 import argparse
 import logging
+import pathlib
 import sys
+
+import numpy as np
 
 import gpsdatetime as gpst
 import gnsstoolbox.orbits as orb
 import gnsstoolbox.gnsstools as tools
 
-import numpy as np
-# import matplotlib.pyplot as plt
-import folium
-import pygmt
-import plotly.express as px
-# from mpl_toolkits.basemap import Basemap
-
 
 # some constants
+EXIT_CODE_FILE_NOT_FOUND = -99
+EXIT_CODE_SV_MISSING = -98
 LABELS_INTERVAL = 50
 
 
-def validate_sv_id(sv_id):
-    """Validate the satellite ID given as command line argument."""
-    # 1st character should denote the GNSS system
-    if not sv_id.startswith(('G', 'R', 'E')):
-        raise argparse.ArgumentTypeError("Invalid SV constellation.")
+# ploting functions
+# *****************
 
-    # the next 2 should be the ID
-    try:
-        id_ = int(sv_id[1:])  # noqa: F841
-    except ValueError:
-        raise argparse.ArgumentTypeError("Invalid SV ID.")
-
-    return sv_id
-
-
-def parse_command_line():
-    """Parse and validate the command line arguments."""
-    parser = argparse.ArgumentParser()
-
-    parser.add_argument('--sp3',
-        metavar='SP3_FILE',
-        dest='sp3',
-        required=True,
-        help='SP3 file; the file to query.')
-
-    parser.add_argument('--sv_id',
-        metavar='SV_ID',
-        dest='sv_id',
-        required=False,
-        type=validate_sv_id,
-        default = None,
-        help='''SV ID; the satellite to plot.
-    Use RINEX v.3 notation eg. G09, R17, E21, etc.
-    If not given, the first satellite in the file will be used (usually G01).''')
-
-    parser.add_argument('--save',
-        required=False,
-        default=False,
-        action='store_true',
-        help='Save ground plot to a PNG file.')
-
-    return parser.parse_args()
-
-
-def plot_track(sv_id, geo, labels, save):
+def plot_track_pygmt(sv_id, geo, labels, save):
     """Plot the satellite's ground track using `pygmt`."""
     pygmt.config(GMT_THEME='modern')
     pygmt.config(FONT_TITLE='9p,Helvetica-Bold,black')
@@ -102,25 +59,13 @@ def plot_track(sv_id, geo, labels, save):
 
     fig.show()
     if save:
-        fig.savefig(f'ground_track_{sv_id}.png', transparent=True)
+        png_file = pathlib.Path(__file__).parents[2]\
+                                         .joinpath('plots')\
+                                         .joinpath(f'ground_track_{sv_id}.png')
+        fig.savefig(png_file, transparent=True)
 
 
-def plot_track_2(sv_id, geo, labels, save):
-    """Plot the satellite's ground track using `folium`."""
-    m = folium.Map(location=[0., 0.], zoom_start=3)
-
-    # Add markers or pop-ups as needed
-    # labels = [x if i % LABELS_INTERVAL == 0 else None for i, x in enumerate(labels)]
-    for p in range(len(geo[0])):
-        m.add_child(folium.Marker(location=[np.degrees(geo[1][p]), np.degrees(geo[0][p])],
-                    popup=labels[p]))
-
-    # Save the map as an HTML file
-    if save:
-        m.save(f'ground_track_{sv_id}.html')
-
-
-def plot_track_3(sv_id, geo, labels, save):
+def plot_track_plotly(sv_id, geo, labels, save):
     """Plot the satellite's ground track using `plotly`."""
     fig = px.scatter_geo(lat=np.degrees(geo[1]),
                          lon=np.degrees(geo[0]),
@@ -129,19 +74,50 @@ def plot_track_3(sv_id, geo, labels, save):
     fig.show()
 
 
-def plot_track_4(sv_id, geo, labels, save):
-    """Plot the satellite's ground track using `plotly`."""
-    fig = px.scatter_map(lat=np.degrees(geo[1]),
-                         lon=np.degrees(geo[0]),
-                         hover_name=labels,
-                         zoom=2,
-                         map_style='satellite-streets',
-# 'basic',
-# 'carto-darkmatter', 'carto-darkmatter-nolabels', 'carto-positron', 'carto-positron-nolabels',
-# 'carto-voyager', 'carto-voyager-nolabels', 'dark', 'light', 'open-street-map', 'outdoors',
-# 'satellite', 'satellite-streets', 'streets', 'white-bg'
-                         title=f'Ground track of SV {sv_id}')
-    fig.show()
+# check ploting package availability
+try:
+    import plotly.express as px
+    plot_track = plot_track_plotly
+except ModuleNotFoundError:
+    import pygmt
+    plot_track = plot_track_pygmt
+
+
+def validate_sv_id(sv_id):
+    """Validate the satellite ID given as command line argument."""
+    # 1st character should denote the GNSS system
+    if not sv_id.startswith(('G', 'R', 'E')):
+        raise argparse.ArgumentTypeError("Invalid SV constellation.")
+
+    # the next 2 should be the ID
+    try:
+        id_ = int(sv_id[1:])  # noqa: F841
+    except ValueError:
+        raise argparse.ArgumentTypeError("Invalid SV ID.")
+
+    return sv_id
+
+
+def parse_command_line():
+    """Parse and validate the command line arguments."""
+    parser = argparse.ArgumentParser(
+        description="Read satellite positions from a SP3 file and plot ground tracks.",
+        epilog="The S/W currently uses `pyGMT` for the plotting, but it's `plotly`-ready.")
+
+    parser.add_argument('sp3',
+        help='the file to query')
+
+    parser.add_argument('--sv_id',
+        type=validate_sv_id,
+        help='''the satellite to plot.
+    Use RINEX v.3 notation eg. G09, R17, E21, etc.
+    If not given, the first satellite in the file will be used (usually G01)''')
+
+    parser.add_argument('-s', '--save',
+        action='store_true',
+        help='save ground plot to a PNG file')
+
+    return parser.parse_args()
 
 
 def main():
@@ -153,7 +129,7 @@ def main():
     failure = orbit.loadSp3(args.sp3)
     if failure:
         logging.error(f"File \'{args.sp3}\' not found.  Exiting.")
-        sys.exit(-1)
+        sys.exit(EXIT_CODE_FILE_NOT_FOUND)
 
     # get the SV info
     try:
@@ -164,6 +140,10 @@ def main():
 
     # get SV positions
     sp3 = orbit.getSp3(*sv_id)
+    # if requested satellite is not present (ie. the length of the orbit is 0), exit
+    if sp3[1] == 0:
+        logging.error(f"Satellite {args.sv_id} not in file.  Exiting.")
+        sys.exit(EXIT_CODE_SV_MISSING)
 
     # time (to use as labels)
     time_ = [gpst.gpsdatetime(mjd=x).st_iso_epoch() for x in sp3[0][:, 0]]
@@ -173,7 +153,7 @@ def main():
     geo = list(tools.toolCartGeoGRS80(*ecef.T))
 
     # plot
-    plot_track(f'{sv_id[0]}{sv_id[1]:02d}', geo, time_, args.save)
+    plot_track(f'{args.sv_id}', geo, time_, args.save)
 
 
 if __name__ == "__main__":
